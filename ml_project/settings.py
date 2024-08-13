d5e54ac069f1b55b660b7a1b2ce974f269bc998f
Georgi <georgi.manev@entra.energy>
2024-08-13 14:49:25 +0300

    init

--- ml_project/settings.py
+++ ml_project/settings.py
@@ -95,7 +95,7 @@ CELERY_RESULT_BACKEND = os.getenv("CELERY_BACKEND", "redis://redis:6379/0")
 CELERY_BEAT_SCHEDULE = {
     'todaty_correlation': {
         'task': 'mlapp.tasks.today_correlation_task',
-        'schedule': crontab(hour=14, minute=40),
+        'schedule': crontab(hour=14, minute=53),
     },
 }
 
