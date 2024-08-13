c7aabfa60e6920cd28d7ae47e65f69ec2724e3bb
Georgi <georgi.manev@entra.energy>
2024-08-13 17:05:48 +0300

    init

--- mlapp/views.py
+++ mlapp/views.py
@@ -2,7 +2,8 @@ from rest_framework import viewsets
 from .models import Forecast, Feature, Correlation
 from .serializers import ForecastSerializer, FeatureSerializer, CorrelationSerializer
 from .tasks import today_correlation_task
-
+from datetime import datetime
+from django.utils import timezone
 
 
 
@@ -12,11 +13,26 @@ class ForecastViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset =  super().get_queryset()
-        dev_id = self.request.query_params.get('devId')
-
+        dev_id = self.request.query_params.get('devId', None)
+        date_range = self.request.query_params.get('date_range',None) 
+        
+       
         # Optionally, filter queryset based on devId
         if dev_id:
             queryset = queryset.filter(devId=dev_id)
+
+        # Filter by date_range if provided
+        if date_range:
+            today = timezone.now().date()
+
+            if date_range == 'today':
+                queryset = queryset.filter(date__date=today)
+            elif date_range == 'month':
+                first_day_of_month = today.replace(day=1)
+                queryset = queryset.filter(date__date__gte=first_day_of_month, date__date__lte=today)
+            elif date_range == 'year':
+                first_day_of_year = today.replace(month=1, day=1)
+                queryset = queryset.filter(date__date__gte=first_day_of_year, date__date__lte=today)
         return queryset
 
 
