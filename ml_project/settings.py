33c732cef2efb698a167e1447742f216ea4b80f9
Georgi <georgi.manev@entra.energy>
2024-08-14 13:49:40 +0300

    init

--- ml_project/settings.py
+++ ml_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '85.14.6.37', '192.168.1.32' '[::1]']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '85.14.6.37', '192.168.1.32']
 
 CORS_ORIGIN_ALLOW_ALL=True
 
