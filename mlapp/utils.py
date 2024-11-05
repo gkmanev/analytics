8707eb5d4867f5039f026db86f9c4c18bbf7b22c
Georgi <georgi.manev@entra.energy>
2024-11-05 16:19:10 +0200

    data_resample

--- mlapp/utils.py
+++ mlapp/utils.py
@@ -8,7 +8,7 @@ def today_correlation():
     with open(file_path, 'r') as file:
         coords = json.load(file)
     
-    for dev in coords[6:7]:
+    for dev in coords[7:10]:
         for k,v in dev.items():
             devId = k 
             lat = v["lat"]
