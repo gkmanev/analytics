58d66246ca87365fd2182782883b58ea81bf99b6
Georgi <georgi.manev@entra.energy>
2025-02-09 12:22:36 +0200

    add PV

--- ml_project/settings.py
+++ ml_project/settings.py
@@ -110,7 +110,7 @@ CELERY_BEAT_SCHEDULE = {
     },
     'pv_forecast_task': {
         'task': 'mlapp.tasks.pv_forecast_task',
-        'schedule': crontab(hour=20, minute=29),
+        'schedule': crontab(hour=12, minute=24),
     },
 }
 
