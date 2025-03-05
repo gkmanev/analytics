b47b3dba7bab960be42b2b0d8b6614ebebfbe067
Georgi <georgi.manev@entra.energy>
2025-03-05 12:50:19 +0200

    up

--- mlapp/pv_forecast.py
+++ mlapp/pv_forecast.py
@@ -198,7 +198,7 @@ class PVForecast:
                     timestamp_column="timestamp"
                 )
 
-                model_save_path = f"/app/data/model_{self.ppe}_{self.end_date}"
+                model_save_path = f"/app/data/model_{self.ppe}_{self.end_date}____"
                 if os.path.exists(model_save_path):
                     predictor = TimeSeriesPredictor.load(model_save_path)
                 else:                
@@ -208,31 +208,31 @@ class PVForecast:
                         prediction_length=480,
                         freq='15min',
                         known_covariates_names=known_covariates,
-                        path=model_save_path  # Set the path here
+                        #path=model_save_path  # Set the path here
                     )
 
                     #Fit the predictor with cross-validation
                     results = predictor.fit(
 
                         train_data=train_data,  
-                        # presets="fast_training",
-                        time_limit=1200,
-                        hyperparameters={
-                            "DeepAR": {
-                                # You can specify DeepAR-specific hyperparameters here
-                                # For example:
-                                "context_length": 576,
-                                "num_layers": 3,
-                                "hidden_size": 480,
-                                "dropout_rate": 0.2,
-                                "learning_rate": 1e-3,
-                                "epochs": 100,  # Added epochs parameter
-                                "callbacks": [EarlyStopping(monitor="val_loss", patience=20, mode="min")]
-                            }
-                        },     
+                        presets="fast_training",
+                        #time_limit=1200,
+                        # hyperparameters={
+                        #     "DeepAR": {
+                        #         # You can specify DeepAR-specific hyperparameters here
+                        #         # For example:
+                        #         "context_length": 576,
+                        #         "num_layers": 3,
+                        #         "hidden_size": 480,
+                        #         "dropout_rate": 0.2,
+                        #         "learning_rate": 1e-3,
+                        #         "epochs": 100,  # Added epochs parameter
+                        #         "callbacks": [EarlyStopping(monitor="val_loss", patience=20, mode="min")]
+                        #     }
+                        # },     
                         
                     )                             
-    
+             
                 predictions = predictor.predict(data=train_data, known_covariates=future_covariates)
                 predictions.reset_index(inplace=True)
                 predictions = predictions[['timestamp', '0.9']]
