a4fae570725175d92812854632cc5c2e3319d043
Georgi <georgi.manev@entra.energy>
2025-03-05 12:57:12 +0200

    up

--- ml_project/settings.py
+++ ml_project/settings.py
@@ -111,7 +111,7 @@ CELERY_BEAT_SCHEDULE = {
     },
     'schedule_pv_forecast_first_five_task': {
         'task': 'mlapp.tasks.pv_forecast_first_five_task',
-        'schedule': crontab(hour=12, minute=52),
+        'schedule': crontab(hour=12, minute=58),
     },
     'schedule_pv_forecast_five_ten_task': {
         'task': 'mlapp.tasks.pv_forecast_five_ten_task',
