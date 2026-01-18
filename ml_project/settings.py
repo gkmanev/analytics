2627e8afa226663e906faa9397667476651f6905
Georgi <georgi.manev@entra.energy>
2026-01-18 16:54:54 +0200

    filters

--- ml_project/settings.py
+++ ml_project/settings.py
@@ -103,7 +103,7 @@ CELERY_BEAT_SCHEDULE = {
 
     'todaty_correlation': {
         'task': 'mlapp.tasks.today_correlation_task',
-        'schedule': crontab(hour=15, minute=55),
+        'schedule': crontab(hour=16, minute=59),
     },
     # # call again after 2 min
     #  'today_correlation_1138': {
